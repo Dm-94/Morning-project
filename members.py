d5576d9cb32273a4904c8021e950ff5f0362bba6
Rose Muthini <rosemuthini78@gmail.com>
2025-03-28 11:05:00 +0300

    push from  git hub.py

--- members.py
+++ members.py
@@ -1,3 +1,4 @@
 print("Welcome branch RoseProject")
 print("This content should only appear in this branch only")
 print("If it appears in other branches i.e main,charity or David, then I should consult why!")
+print("Testing double pushing on the side of git hub")
